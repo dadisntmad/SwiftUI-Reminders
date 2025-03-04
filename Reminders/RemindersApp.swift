a8b76ca18072c42f0ae9f0af080aa2b2f2dc5c9a
Ruslan <ruslan.lantsov.m@gmail.com>
2025-03-04 18:52:08 +0200

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Ruslan Lantsov on 04.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
